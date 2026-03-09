9e7c4951738b9b540315601cf91940ce779da58c
DuongquangVu <duongquangvu1707@gmail.com>
2026-03-09 10:21:53 +0700

    add test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// Simple JavaScript Script
+
+const name = "World";
+console.log(`Hello, ${name}!`);
+
+// Basic arithmetic
+const a = 10;
+const b = 5;
+console.log(`Sum: ${a + b}`);
+console.log(`Difference: ${a - b}`);
+console.log(`Product: ${a * b}`);
+console.log(`Quotient: ${a / b}`);
+
+// Simple array
+const fruits = ["Apple", "Banana", "Cherry"];
+fruits.forEach((fruit, index) => {
+  console.log(`${index + 1}. ${fruit}`);
+});
